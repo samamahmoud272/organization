32ed8841b07c61ba23ffeda20fc9b277faf0f125
mjamal <mjamal@aas.com.sa>
2024-10-27 02:50:47 +0300

    adding docker changes

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 class Config:
-    MONGO_URI = "mongodb://localhost:27017/orgdb"  
+    # MONGO_URI = "mongodb://localhost:27017/orgdb"  for locally run with command -- flask run
+    MONGO_URI = "mongodb://mongo:27017/orgdb" # for docker running
     JWT_SECRET_KEY = "1c119451894c0a7b45bd2854182ba76fbd5d4cc418fee2421f4eb27a71266487"  
     JWT_ACCESS_TOKEN_EXPIRES = 3600  # Token expiration time  (1 hour)
     JWT_REFRESH_TOKEN_EXPIRES = 604800  # Refresh token expiration time (1 week)
--- dockerfile
+++ dockerfile
@@ -1,10 +1,13 @@
-FROM python:3.12-slim
+FROM python:3.12
 
 WORKDIR /app
 
 COPY requirements.txt .
-RUN pip install -r requirements.txt
+
+RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["flask", "run", "--host=0.0.0.0", "--port=5000"]
+EXPOSE 8080
+
+CMD ["flask", "run", "--host=0.0.0.0", "--port=8080"]
